4e6e73ee3c93a00b70d138d233d2e2060f44ef9f
dr-js <dr@dr.city>
2017-09-22 01:18:33 +0800

    update OptionParser

--- source/index.js
+++ source/index.js
@@ -8,59 +8,61 @@ import { parseCLI, parseENV, parseJSON, processOptionMap, exitWithError } from '
 const readFileAsync = promisify(nodeModuleFs.readFile)
 
 const main = async () => {
-  let optionMap = parseCLI(process.argv)
-  const getOption = (name) => optionMap[ name ] && optionMap[ name ].argumentList[ 0 ]
+  let optionMap = optionMapResolvePath(parseCLI(process.argv), process.cwd())
 
-  const config = getOption('config')
-  let pathRelative
-  if (!config) {
-    __DEV__ && console.log('all cli')
-    pathRelative = process.cwd() // relative to the path cwd
-  } else if (config.toLowerCase() === 'env') {
-    __DEV__ && console.log('merge env')
-    optionMap = { ...parseENV(process.env), ...optionMap }
-    pathRelative = process.cwd() // relative to the path cwd
-  } else {
-    __DEV__ && console.log('merge json', config)
-    optionMap = { ...parseJSON(JSON.parse(await readFileAsync(config, 'utf8'))), ...optionMap }
-    pathRelative = nodeModulePath.dirname(config) // relative to packager-config.json
+  const getSingleOption = (name) => getSingleOptionOptional(name) || exitWithError(new Error(`[option] missing option ${name}`))
+  const getSingleOptionOptional = (name) => optionMap[ name ] && optionMap[ name ].argumentList[ 0 ]
+
+  const config = getSingleOptionOptional('config')
+  if (config && config.toLowerCase() === 'env') {
+    const envOptionMap = optionMapResolvePath(parseENV(process.env), process.cwd())
+    optionMap = { ...envOptionMap, ...optionMap }
+  } else if (config) {
+    const jsonOptionMap = optionMapResolvePath(parseJSON(JSON.parse(await readFileAsync(config, 'utf8'))), nodeModulePath.dirname(config))
+    optionMap = { ...jsonOptionMap, ...optionMap }
   }
 
-  const mode = getOption('mode')
-  const protocol = getOption('type')
+  __DEV__ && console.log('[option]')
+  __DEV__ && Object.keys(optionMap).forEach((name) => console.log(`  - [${name}] ${getSingleOption(name)}`))
 
-  __DEV__ && Object.keys(optionMap).forEach((name) => console.log(`[${name}] ${getOption(name)}`))
   optionMap = processOptionMap(optionMap)
   __DEV__ && console.log('processOptionMap PASS')
 
+  const mode = getSingleOption('mode')
+  const hostName = getSingleOption('host-name')
+  const pathResource = getSingleOption('path-resource')
+
   if (mode === 'server') {
+    const protocol = getSingleOption('type')
     const { start } = await configureServer({
       protocol,
-      hostName: getOption('host-name'),
-      port: getOption('port'),
+      hostName,
+      port: getSingleOption('port'),
       ...(protocol === 'HTTPS' ? {
-        fileSSLKey: getOption('file-SSL-key'),
-        fileSSLCert: getOption('file-SSL-cert'),
-        fileSSLChain: getOption('file-SSL-chain'),
-        fileSSLDHParam: getOption('file-SSL-dhparam')
+        fileSSLKey: getSingleOption('file-SSL-key'),
+        fileSSLCert: getSingleOption('file-SSL-cert'),
+        fileSSLChain: getSingleOption('file-SSL-chain'),
+        fileSSLDHParam: getSingleOption('file-SSL-dhparam')
       } : {}),
-      fileFirebaseAdminToken: getOption('file-firebase-admin-token'),
-      pathResource: nodeModulePath.resolve(pathRelative, getOption('path-resource')),
-      pathLog: nodeModulePath.resolve(pathRelative, getOption('path-log')),
-      logFilePrefix: getOption('prefix-log-file')
+      fileFirebaseAdminToken: getSingleOption('file-firebase-admin-token'),
+      pathResource,
+      pathLog: getSingleOption('path-log'),
+      logFilePrefix: getSingleOption('prefix-log-file')
     })
     return start()
   }
 
   if (mode === 'certbot') {
-    const { start } = await configureServerCertBot({
-      hostName: getOption('host-name'),
-      pathResource: nodeModulePath.resolve(pathRelative, getOption('path-resource'))
-    })
+    const { start } = await configureServerCertBot({ hostName, pathResource })
     return start()
   }
 
-  throw new Error(`[main] error mode: ${mode}`)
+  throw new Error(`[main] strange error`)
+}
+
+const optionMapResolvePath = (optionMap, pathRelative) => {
+  Object.values(optionMap).forEach(({ format: { isPath }, argumentList }) => isPath && argumentList.forEach((v, i) => (argumentList[ i ] = nodeModulePath.resolve(pathRelative, v))))
+  return optionMap
 }
 
 main().catch(exitWithError)
--- source/option.js
+++ source/option.js
@@ -1,21 +1,12 @@
 import { Common } from 'dr-js/module/Dr.node'
 const { createOptionParser, OPTION_CONFIG_PRESET } = Common.Module
 
+const checkModeServerOption = (optionMap, optionFormatSet, format) => optionMap[ 'mode' ].argumentList[ 0 ] !== 'server'
+const checkTypeHttpsOption = (optionMap, optionFormatSet, format) => optionMap[ 'type' ].argumentList[ 0 ] !== 'HTTPS'
+
 const OPTION_CONFIG = {
   prefixENV: 'dr-city',
   formatList: [
-    {
-      name: 'mode',
-      shortName: 'm',
-      description: `should be 'server' or 'certbot'`,
-      ...OPTION_CONFIG_PRESET.OneOfString([ 'server', 'certbot' ])
-    },
-    {
-      name: 'type',
-      shortName: 't',
-      description: `protocol type, 'HTTPS' or 'HTTP'`,
-      ...OPTION_CONFIG_PRESET.OneOfString([ 'HTTPS', 'HTTP' ])
-    },
     {
       name: 'config',
       shortName: 'c',
@@ -23,16 +14,32 @@ const OPTION_CONFIG = {
       description: `# from JSON: set to path relative process.cwd()\n# from ENV: set to 'env' to collect from process.env`,
       ...OPTION_CONFIG_PRESET.SingleString
     },
+    {
+      name: 'mode',
+      shortName: 'm',
+      description: `should be 'server' or 'certbot'`,
+      ...OPTION_CONFIG_PRESET.OneOfString([ 'server', 'certbot' ]),
+      extendFormatList: [
+        { name: 'file-firebase-admin-token', optional: checkModeServerOption, ...OPTION_CONFIG_PRESET.SingleString, isPath: true },
+        { name: 'path-log', optional: checkModeServerOption, ...OPTION_CONFIG_PRESET.SingleString, isPath: true },
+        { name: 'prefix-log-file', optional: checkModeServerOption, ...OPTION_CONFIG_PRESET.SingleString }
+      ]
+    },
+    {
+      name: 'type',
+      shortName: 't',
+      description: `protocol type, 'HTTPS' or 'HTTP'`,
+      ...OPTION_CONFIG_PRESET.OneOfString([ 'HTTPS', 'HTTP' ]),
+      extendFormatList: [
+        { name: 'file-SSL-key', optional: checkTypeHttpsOption, ...OPTION_CONFIG_PRESET.SingleString, isPath: true },
+        { name: 'file-SSL-cert', optional: checkTypeHttpsOption, ...OPTION_CONFIG_PRESET.SingleString, isPath: true },
+        { name: 'file-SSL-chain', optional: checkTypeHttpsOption, ...OPTION_CONFIG_PRESET.SingleString, isPath: true },
+        { name: 'file-SSL-dhparam', optional: checkTypeHttpsOption, ...OPTION_CONFIG_PRESET.SingleString, isPath: true }
+      ]
+    },
     { name: 'host-name', shortName: 'h', ...OPTION_CONFIG_PRESET.SingleString },
     { name: 'port', shortName: 'p', ...OPTION_CONFIG_PRESET.SingleInteger },
-    { name: 'file-SSL-key', optional: true, ...OPTION_CONFIG_PRESET.SingleString },
-    { name: 'file-SSL-cert', optional: true, ...OPTION_CONFIG_PRESET.SingleString },
-    { name: 'file-SSL-chain', optional: true, ...OPTION_CONFIG_PRESET.SingleString },
-    { name: 'file-SSL-dhparam', optional: true, ...OPTION_CONFIG_PRESET.SingleString },
-    { name: 'file-firebase-admin-token', ...OPTION_CONFIG_PRESET.SingleString },
-    { name: 'path-log', ...OPTION_CONFIG_PRESET.SingleString },
-    { name: 'prefix-log-file', ...OPTION_CONFIG_PRESET.SingleString },
-    { name: 'path-resource', ...OPTION_CONFIG_PRESET.SingleString }
+    { name: 'path-resource', ...OPTION_CONFIG_PRESET.SingleString, isPath: true }
   ]
 }
 
